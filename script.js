422448f2bf55ac07df9d8b0e9a94887f4e88dcc7
mianazan099 <103571768+mianazan099@users.noreply.github.com>
2022-10-25 11:49:06 +0500

    javascript completed

--- script.js
+++ script.js
@@ -1,105 +1,119 @@
+"use strict";
 const template = document.querySelector("template");
 const parent = document.querySelector(".parent");
+const searchTablets = document.querySelector(".search-tablets");
+const clearBtn = document.querySelector(".clear");
+const tabletList = document.querySelectorAll(".tablet");
+const searchList = [];
+function removeCards() {
+  const cardList = document.querySelectorAll(".card");
+  cardList.forEach((card) => card.remove());
+}
 
-function filterData(filterList) {
+function createCards(data) {
+  const cardsArray = [];
+  data.forEach((data) => {
+    const newCard = template.content.cloneNode(true);
+    newCard.querySelector(".logo img").src = data.logo;
+    newCard.querySelector(".company").textContent = data.company;
+    newCard.querySelector(".position").textContent = data.position;
+    newCard.querySelector(".postedAt").textContent = data.postedAt;
+    newCard.querySelector(".contract").textContent = data.contract;
+    newCard.querySelector(".location").textContent = data.location;
+    if (!data.featured) {
+      newCard.querySelector(".featured").remove();
+      newCard.querySelector(".card").classList.remove("right-line");
+    }
+    if (!data.new) newCard.querySelector(".new").remove();
+    data.tablets.forEach((tablet) => {
+      let newTablet = newCard.querySelector(".tablet").cloneNode(true);
+      newTablet.textContent = tablet;
+      newCard.querySelector(".tablet-container").appendChild(newTablet);
+    });
+    newCard.querySelector(".tablet:first-of-type").remove();
+    cardsArray.push(newCard);
+  });
+  return cardsArray;
+}
+
+function filterData(searchList) {
   let newData = data.filter((item) => {
     let doReturn = true;
     let num = 0;
-    while (doReturn && num < (filterList.length || 1)) {
-      doReturn = item.tablets.includes(filterList[num]);
+    while (doReturn && num < searchList.length) {
+      doReturn = item.tablets.includes(searchList[num]);
       num++;
     }
     return doReturn;
   });
-  if (filterList.length === 0) {
-    render(data);
-  } else {
-    render(newData);
-  }
-}
-function render(data) {
-  const cardList = document.querySelectorAll(".card");
-  cardList.forEach((card) => card.remove());
-  data.forEach((data) => {
-    const clone = template.content.cloneNode(true);
-    clone.querySelector(".logo img").src = data.logo;
-    clone.querySelector(".company").innerText = data.company;
-    if (!data.new) clone.querySelector(".new").remove();
-    if (!data.featured) clone.querySelector(".featured").remove();
-    clone.querySelector(".position").innerText = data.position;
-    clone.querySelector(".postedAt").innerText = data.postedAt;
-    clone.querySelector(".contract").innerText = data.contract;
-    clone.querySelector(".location").innerText = data.location;
-    // console.clear();
-    data.tablets.forEach((tablet) => {
-      let newTablet = clone.querySelector(".tablet").cloneNode(true);
-      newTablet.innerText = tablet;
-      clone.querySelector(".tablet-container").appendChild(newTablet);
-    });
-    clone.querySelector(".tablet:first-of-type").remove();
-    parent.appendChild(clone);
-    if (data.featured) {
-      document
-        .querySelectorAll(".card")
-        .forEach((card) => card.classList.add("right-line"));
-    }
-  });
-  const tabletList = document.querySelectorAll(".tablet");
-  tabletList.forEach((tablet) => {
-    tablet.addEventListener("click", function () {
-      if (!filterList.includes(tablet.textContent))
-        filterList.push(tablet.textContent);
-      pushNow();
-      filterData(filterList);
-    });
-  });
+  renderCards(newData);
 }
 
-render(data);
-
-const tabletList = document.querySelectorAll(".tablet");
-let filterList = [];
-const tablets = document.querySelector(".tablets");
-
-tabletList.forEach((tablet) => {
-  tablet.addEventListener("click", function () {
-    if (!filterList.includes(tablet.textContent))
-      filterList.push(tablet.textContent);
-    pushNow();
-    filterData(filterList);
-  });
-});
-
-function pushNow() {
-  tablets.innerHTML = "";
-  filterList.forEach((item) => {
-    tablets.innerHTML += `<div>${item}</div>`;
-  });
-  const tabletDiv = document.querySelectorAll(".tablets div");
+function addSearchTabletListener() {
+  const tabletDiv = document.querySelectorAll(".search-tablets div");
   tabletDiv.forEach((tablet) => {
     tablet.addEventListener("click", () => {
       tablet.remove();
-      const index = filterList.indexOf(tablet.textContent);
-      console.log("no");
+      const index = searchList.indexOf(tablet.textContent);
       if (index > -1) {
-        filterList.splice(index, 1);
-        console.log("done");
+        searchList.splice(index, 1);
       }
-      console.log(filterList);
-      filterData(filterList);
+      filterData(searchList);
+      if (searchList.length === 0) {
+        parent.classList.remove("show-search");
+      }
     });
   });
 }
 
-const clearBtn = document.querySelector(".clear");
+function pushNow() {
+  const searchTabletsList = createSearchTablets();
+  searchTablets.innerHTML = "";
+  searchTabletsList.forEach((item) => {
+    searchTablets.innerHTML += item;
+    parent.classList.add("show-search");
+  });
+  addSearchTabletListener();
+}
 
-function clear() {
-  const tabletDiv = document.querySelectorAll(".tablets div");
-  tabletDiv.forEach((tablet) => {
-    tablet.remove();
-    filterList = [];
-    filterData(filterList);
+function addTabletListener() {
+  const tabletList = document.querySelectorAll(".tablet");
+  tabletList.forEach((tablet) => {
+    tablet.addEventListener("click", function () {
+      if (!searchList.includes(tablet.textContent))
+        searchList.push(tablet.textContent);
+      pushNow();
+      filterData(searchList);
+    });
   });
 }
 
-clearBtn.addEventListener("click", clear);
+function createSearchTablets() {
+  const searchTablets = [];
+  searchList.forEach((item) => {
+    searchTablets.push(`<div>${item}</div>`);
+  });
+  return searchTablets;
+}
+
+function clear() {
+  const tabletDiv = document.querySelectorAll(".search-tablets div");
+  tabletDiv.forEach((tablet) => {
+    tablet.remove();
+    searchList.length = 0;
+    renderCards(data);
+    parent.classList.remove("show-search");
+  });
+}
+
+function renderCards(data) {
+  removeCards();
+  let cardsArray = createCards(data);
+  cardsArray.forEach((card) => {
+    parent.appendChild(card);
+  });
+  addTabletListener();
+  clearBtn.addEventListener("click", clear);
+}
+
+renderCards(data);
